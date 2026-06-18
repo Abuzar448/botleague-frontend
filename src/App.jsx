7527f4c3fd5a9d9c6dee761c32ec5a1792c4f10f
Abuzar khan <aburaja2654@gmail.com>
2026-06-18 14:28:55 +0530

    Fix: Hardlocked tournament bracket connecting wires via absolute coordinates grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Competitions from './components/Compitions&Events';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Competitions></Competitions>
     </div>
   )
 }
